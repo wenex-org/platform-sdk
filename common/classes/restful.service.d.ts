221973c18a2840dcfd93d129d34a3d93fb0cb2bd
Vahid V. <vhid.vz@gmail.com>
2023-12-03 16:01:58 +0330

    update

--- common/classes/restful.service.d.ts
+++ common/classes/restful.service.d.ts
@@ -1,19 +1,29 @@
 import { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { Dto as DtoInterface, Core, Filter, QueryFilter, Serializer } from '../interfaces';
+import { Dto as DtoInterface, Core, Filter, QueryFilter, Serializer, Pagination, Optional } from '../interfaces';
 import { RequestService } from '../providers';
+export type RequestConfig<T> = Omit<AxiosRequestConfig, 'params'> & {
+    params?: Optional<Filter<T>> & {
+        zone?: string;
+        skip?: number;
+        limit?: number;
+        sort?: Pagination<T>['sort'];
+    } & {
+        [x: string]: any;
+    };
+};
 export declare class RestfulService<T extends Core, Dto extends DtoInterface<Core>> extends RequestService {
     protected readonly name: string;
     protected readonly axios: AxiosInstance;
     protected readonly url: (path: string) => string;
     constructor(name: string, axios: AxiosInstance);
-    count(filter: QueryFilter<T>, config?: AxiosRequestConfig): Promise<number>;
-    create(data: Dto, config?: AxiosRequestConfig): Promise<Serializer<T>>;
-    createBulk(data: Dto[], config?: AxiosRequestConfig): Promise<Serializer<T>[]>;
-    find(filter: Filter<T>, config?: AxiosRequestConfig): Promise<Serializer<T>[]>;
-    findById(id: string, config?: AxiosRequestConfig): Promise<Serializer<T>>;
-    deleteById(id: string, config?: AxiosRequestConfig): Promise<Serializer<T>>;
-    restoreById(id: string, config?: AxiosRequestConfig): Promise<Serializer<T>>;
-    destroyById(id: string, config?: AxiosRequestConfig): Promise<Serializer<T>>;
-    updateById(id: string, data: Dto, config?: AxiosRequestConfig): Promise<Serializer<T>>;
-    updateBulk(data: Dto, filter: QueryFilter<T>, config?: AxiosRequestConfig): Promise<number>;
+    count(filter: QueryFilter<T>, config?: RequestConfig<T>): Promise<number>;
+    create(data: Dto, config?: RequestConfig<T>): Promise<Serializer<T>>;
+    createBulk(data: Dto[], config?: RequestConfig<T>): Promise<Serializer<T>[]>;
+    find(filter: Filter<T>, config?: RequestConfig<T>): Promise<Serializer<T>[]>;
+    findById(id: string, config?: RequestConfig<T>): Promise<Serializer<T>>;
+    deleteById(id: string, config?: RequestConfig<T>): Promise<Serializer<T>>;
+    restoreById(id: string, config?: RequestConfig<T>): Promise<Serializer<T>>;
+    destroyById(id: string, config?: RequestConfig<T>): Promise<Serializer<T>>;
+    updateById(id: string, data: Dto, config?: RequestConfig<T>): Promise<Serializer<T>>;
+    updateBulk(data: Dto, filter: QueryFilter<T>, config?: RequestConfig<T>): Promise<number>;
 }
