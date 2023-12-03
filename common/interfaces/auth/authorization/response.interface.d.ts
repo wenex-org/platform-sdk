760d3b619ea8cbe74b04e744f63f748177f7a248
Vahid V. <vhid.vz@gmail.com>
2023-12-03 12:40:11 +0330

    update

--- common/interfaces/auth/authorization/response.interface.d.ts
+++ common/interfaces/auth/authorization/response.interface.d.ts
@@ -1,6 +1,7 @@
 import { Policy } from './policy.interface';
 export interface AuthorizationCanResponse {
     granted: boolean;
+    policies: Policy[];
 }
 export interface AuthorizationPolicyResponse {
     policies: Policy[];
