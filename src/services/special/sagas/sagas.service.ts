1b7d3392fbbc3bf4bb0ec31a963614b91c485512
Vahid V. <vhid.vz@gmail.com>
2024-10-13 14:34:24 +0330

    update

--- src/services/special/sagas/sagas.service.ts
+++ src/services/special/sagas/sagas.service.ts
@@ -1,8 +1,16 @@
 import type { AxiosInstance } from 'axios';
 
-import { RestfulService } from '../../../common/classes';
+import {
+  Data,
+  Saga,
+  SagaDto,
+  SagaHistory,
+  SagaHistoryAppendDto,
+  SagaStartDto,
+  Serializer,
+} from '../../../common/interfaces';
 import { SagaHistoriesService } from './history.service';
-import { Saga, SagaDto } from '../../../common/interfaces';
+import { RequestConfig, RestfulService } from '../../../common/classes';
 
 export class SagasService extends RestfulService<Saga, SagaDto> {
   protected $histories?: SagaHistoriesService;
@@ -13,5 +21,51 @@ export class SagasService extends RestfulService<Saga, SagaDto> {
 
   get histories() {
     return (this.$histories = this.$histories ?? new SagaHistoriesService(this.axios));
+  }
+
+  async start(
+    data: SagaStartDto,
+    config?: RequestConfig<Saga>,
+  ): Promise<Serializer<Saga>> {
+    return (
+      await this.post<Data<Serializer<Saga>>, SagaStartDto>(
+        this.url('start'),
+        data,
+        config,
+      )
+    ).data;
+  }
+
+  async append(
+    data: SagaHistoryAppendDto,
+    config?: RequestConfig<SagaHistory>,
+  ): Promise<Serializer<SagaHistory>> {
+    return (
+      await this.post<Data<Serializer<SagaHistory>>, SagaHistoryAppendDto>(
+        this.url('append'),
+        data,
+        config,
+      )
+    ).data;
+  }
+
+  async commit(id: string, config?: RequestConfig<Saga>): Promise<Serializer<Saga>> {
+    return (
+      await this.patch<Data<Serializer<Saga>>, undefined>(
+        this.url(`${id}/commit`),
+        undefined,
+        config,
+      )
+    ).data;
+  }
+
+  async abort(id: string, config?: RequestConfig<Saga>): Promise<Serializer<Saga>> {
+    return (
+      await this.patch<Data<Serializer<Saga>>, undefined>(
+        this.url(`${id}/abort`),
+        undefined,
+        config,
+      )
+    ).data;
   }
 }
