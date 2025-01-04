f9762b9a51b14ee5d71b8afa28c84097c6f2a433
Vahid V. <vhid.vz@gmail.com>
2025-01-04 16:12:45 +0330

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,10 +25,14 @@ export default tseslint.config(
   },
   {
     rules: {
-      'prettier/prettier': "off",
+      'prettier/prettier': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off'
     },
   },
 );
--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ export default {
   coveragePathIgnorePatterns: ['/node_modules/'],
   coverageProvider: 'v8',
   preset: 'ts-jest',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/test', '<rootDir>/src'],
   testEnvironment: 'node',
   coverageReporters: ['json-summary', 'text', 'lcov'],
   watchman: true,
