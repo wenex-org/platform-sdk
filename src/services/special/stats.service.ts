9683486e422b80b9c44e914f8ce20526193e7390
Vahid V. <vhid.vz@gmail.com>
2025-02-18 21:58:10 +0330

    update

--- src/services/special/stats.service.ts
+++ src/services/special/stats.service.ts
@@ -1,6 +1,6 @@
 import type { AxiosInstance } from 'axios';
 
-import { Items, Serializer } from '../../common/core/interfaces';
+import { Items, Result, Serializer } from '../../common/core/interfaces';
 import { RequestConfig, RestfulService } from '../../common/core/classes';
 import { Stat, StatCollectDto, StatDto } from '../../common/interfaces/special';
 
@@ -15,6 +15,10 @@ export class StatsService<Properties extends object = object> extends RestfulSer
   ): Promise<Serializer<Stat<Properties>>[]> {
     return (await this.post<Items<Serializer<Stat<Properties>>>, StatCollectDto<Properties>>(this.url('collect'), data, config))
       .items;
+  }
+
+  async stackup(data: StatCollectDto<Properties>, config?: RequestConfig<Stat<Properties>>): Promise<Result> {
+    return await this.post<Result, StatCollectDto<Properties>>(this.url('stackup'), data, config);
   }
 
   static build<Properties extends object = object>(axios: AxiosInstance) {
