94fc1a7dfed695ef70f6af7b3cb7893ea9e31ae0
Vahid V. <vhid.vz@gmail.com>
2025-06-09 07:51:38 +0330

    fix: update remote repository paths in git-remote-add.sh script

--- scripts/git-remote-add.sh
+++ scripts/git-remote-add.sh
@@ -17,60 +17,20 @@ fi
 echo "Checking out to main branch..."
 npm run git checkout main && echo ""
 
-git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform.git"
+git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-sdk.git"
 if [ $? -ne 0 ]; then
-  echo "Error: Failed to add remote repository for platform."
+  echo "Error: Failed to add remote repository for platform-sdk."
 else
   git push "$STAGE" main
-  echo "Remote repository for platform added successfully."
+  echo "Remote repository for platform-sdk added successfully."
 fi
 
-# Navigate to the apps/services directory of the project
-cd "apps/services"
-git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-services.git"
+# Navigate to the src/common directory of the project
+cd "src/common"
+git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-sdk-common.git"
 if [ $? -ne 0 ]; then
-  echo "Error: Failed to add remote repository for platform-services."
+  echo "Error: Failed to add remote repository for platform-sdk-common."
 else
   git push "$STAGE" main
-  echo "Remote repository for platform-services added successfully."
-fi
-
-# Navigate to the apps/workers directory of the project
-cd "../workers"
-git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-workers.git"
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to add remote repository for platform-workers."
-else
-  git push "$STAGE" main
-  echo "Remote repository for platform-workers added successfully."
-fi
-
-# Navigate to the docker directory of the project
-cd "../../docker"
-git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-docker.git"
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to add remote repository for platform-docker."
-else
-  git push "$STAGE" main
-  echo "Remote repository for platform-docker added successfully."
-fi
-
-# Navigate to the libs directory of the project
-cd "../libs"
-git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-libs.git"
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to add remote repository for platform-libs."
-else
-  git push "$STAGE" main
-  echo "Remote repository for platform-libs added successfully."
-fi
-
-# Navigate to the protos directory of the project
-cd "../protos"
-git remote add "$STAGE" "git@gitlab.$DOMAIN:wenex/platform-protos.git"
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to add remote repository for platform-protos."
-else
-  git push "$STAGE" main
-  echo "Remote repository for platform-protos added successfully."
+  echo "Remote repository for platform-sdk-common added successfully."
 fi
