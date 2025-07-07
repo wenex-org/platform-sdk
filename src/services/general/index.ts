da3ab831da91601d294133dac9a6cbb255ad659f
Vahid V. <vhid.vz@gmail.com>
2025-07-07 15:34:57 +0330

    update

--- src/services/general/index.ts
+++ src/services/general/index.ts
@@ -1,19 +1,26 @@
 import type { AxiosInstance } from 'axios';
 
+import { EventsService } from './events.service';
 import { ArtifactsService } from './artifacts.service';
 import { WorkflowsService } from './workflows.service';
 import { ActivitiesService } from './activities.service';
 
+export * from './events.service';
 export * from './artifacts.service';
 export * from './workflows.service';
 export * from './activities.service';
 
 export class Client<Properties extends object = object> {
+  protected _events?: EventsService<Properties>;
   protected _artifacts?: ArtifactsService<Properties>;
   protected _workflows?: WorkflowsService<Properties>;
   protected _activities?: ActivitiesService<Properties>;
 
   constructor(readonly axios: AxiosInstance) {}
+
+  get events() {
+    return (this._events = this._events ?? EventsService.build<Properties>(this.axios));
+  }
 
   get artifacts() {
     return (this._artifacts = this._artifacts ?? ArtifactsService.build<Properties>(this.axios));
