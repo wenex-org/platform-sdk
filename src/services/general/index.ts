539677d3b8518dd5b65901e179bd068c29799314
Vahid V. <vhid.vz@gmail.com>
2025-07-07 10:56:31 +0330

    update

--- src/services/general/index.ts
+++ src/services/general/index.ts
@@ -1,26 +1,19 @@
 import type { AxiosInstance } from 'axios';
 
-import { NoticesService } from './notices.service';
 import { ArtifactsService } from './artifacts.service';
 import { WorkflowsService } from './workflows.service';
 import { ActivitiesService } from './activities.service';
 
-export * from './notices.service';
 export * from './artifacts.service';
 export * from './workflows.service';
 export * from './activities.service';
 
 export class Client<Properties extends object = object> {
-  protected _notices?: NoticesService<Properties>;
   protected _artifacts?: ArtifactsService<Properties>;
   protected _workflows?: WorkflowsService<Properties>;
   protected _activities?: ActivitiesService<Properties>;
 
   constructor(readonly axios: AxiosInstance) {}
-
-  get notices() {
-    return (this._notices = this._notices ?? NoticesService.build<Properties>(this.axios));
-  }
 
   get artifacts() {
     return (this._artifacts = this._artifacts ?? ArtifactsService.build<Properties>(this.axios));
--- src/services/touch/notices.service.ts
+++ src/services/touch/notices.service.ts
@@ -1,11 +1,11 @@
 import type { AxiosInstance } from 'axios';
 
 import { RestfulService } from '../../common/core/classes';
-import { Notice, NoticeDto } from '../../common/interfaces/general';
+import { Notice, NoticeDto } from '../../common/interfaces/touch';
 
 export class NoticesService<Properties extends object = object> extends RestfulService<Notice<Properties>, NoticeDto<Properties>> {
   constructor(protected axios: AxiosInstance) {
-    super('general/notices', axios);
+    super('touch/notices', axios);
   }
 
   static build<Properties extends object = object>(axios: AxiosInstance) {
