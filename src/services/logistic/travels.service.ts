113fbdea858493aa71f1fc94e5a692c961c2d317
m-hadi-arefi <mhadi.arefi82@gmail.com>
2026-02-01 18:18:07 +0330

    add routing to travel service

--- src/services/logistic/travels.service.ts
+++ src/services/logistic/travels.service.ts
@@ -2,6 +2,9 @@ import type { AxiosInstance } from 'axios';
 
 import { RestfulService } from '../../common/core/classes';
 import { Travel, TravelDto } from '../../common/interfaces/logistic';
+import { RequestConfig } from '../../common/core/types';
+import { Routing } from '../../common/interfaces/logistic/travels';
+import { Data } from '../../common/core/interfaces';
 
 export class TravelsService<Properties extends object = object> extends RestfulService<Travel<Properties>, TravelDto<Properties>> {
   constructor(protected axios: AxiosInstance) {
@@ -10,5 +13,9 @@ export class TravelsService<Properties extends object = object> extends RestfulS
 
   static build<Properties extends object = object>(axios: AxiosInstance) {
     return new TravelsService<Properties>(axios);
+  }
+
+  async routing(data: Routing<Properties>, config?: RequestConfig): Promise<Routing<Properties>> {
+    return (await this.post<Data<Routing<Properties>>, Routing<Properties>>(this.url('routing'), data, config)).data;
   }
 }
